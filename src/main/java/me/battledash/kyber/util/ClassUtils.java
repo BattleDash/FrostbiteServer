4eeeb83dd17527246110aaa0100605f3ccc0dd7f
BattleDash <itsbattledash@gmail.com>
2022-08-27 23:30:06 -0600

    Use Guava for ClassUtils.findAllClasses

--- src/main/java/me/battledash/kyber/util/ClassUtils.java
+++ src/main/java/me/battledash/kyber/util/ClassUtils.java
@@ -1,40 +1,37 @@
 package me.battledash.kyber.util;
 
+import com.google.common.reflect.ClassPath;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class ClassUtils {
 
     public static List<Class<?>> findAllClasses(String packageName) {
         List<Class<?>> classes = new ArrayList<>();
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        String line;
+
+        ClassPath classPath;
         try {
-            while ((line = reader.readLine()) != null) {
-                if (line.endsWith(".class")) {
-                    String className = line.substring(0, line.lastIndexOf('.'));
-                    className = className.replaceAll("/", ".");
-                    Class<?> clazz = getClass(className, packageName);
-                    if (clazz != null) {
-                        classes.add(clazz);
-                    }
-                } else {
-                    String subPackageName = line.substring(0, line.lastIndexOf('/') == -1 ? line.length() : line.lastIndexOf('/'));
-                    subPackageName = subPackageName.replaceAll("/", ".");
-                    classes.addAll(findAllClasses(packageName + "." + subPackageName));
-                }
-            }
-        } catch (Exception e) {
+            classPath = ClassPath.from(ClassLoader.getSystemClassLoader());
+        } catch (IOException e) {
             log.error("Error while reading class names from package: {}", packageName, e);
+            return classes;
         }
+
+        for (ClassPath.ClassInfo classInfo : classPath.getAllClasses()) {
+            if (classInfo.getPackageName().startsWith(packageName)) {
+                classes.add(classInfo.load());
+            }
+        }
+
         return classes;
     }
 
